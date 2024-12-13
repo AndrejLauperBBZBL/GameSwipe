052594392c52d1540865cdecaac1212ee85d9e7d
AndrejLauperBBZBL <andrej.lauper@bbzbl-it.ch>
2024-12-13 11:33:48 +0100

    Finished Product

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
+import { PushNotifications, Token } from '@capacitor/push-notifications';
+import { LocalNotifications } from '@capacitor/local-notifications';
 
 @Component({
   selector: 'app-root',
@@ -7,6 +9,56 @@ import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
   standalone: true,
   imports: [IonApp, IonRouterOutlet],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   constructor() {}
+
+  async ngOnInit() {
+    // await this.initPushNotifications();
+    await this.requestNotificationPermissions();
+    await this.scheduleDailyNotification();
+  }
+
+  private async initPushNotifications() {
+    const permission = await PushNotifications.requestPermissions();
+    if (permission.receive === 'granted') {
+      await PushNotifications.register();
+    }
+
+  }
+
+  private async requestNotificationPermissions() {
+    const permission = await LocalNotifications.requestPermissions();
+
+    if (permission.display === 'granted') {
+      console.log('Notification permissions granted');
+    } else {
+      console.error('Notification permissions not granted');
+    }
+  }
+
+  private async scheduleDailyNotification() {
+    await LocalNotifications.cancel({ notifications: [{ id: 1 }] });
+
+    await LocalNotifications.schedule({
+      notifications: [
+        {
+          id: 1,
+          title: 'GameSwipe Reminder',
+          body: 'Start Swiping ;)',
+          schedule: {
+            repeats: true,
+            on: {
+              hour: 17,
+              minute: 0,
+            },
+          },
+          extra: {
+            data: 'Swipe Notification',
+          },
+        },
+      ],
+    });
+
+    console.log('Daily notification scheduled at 17:00');
+  }
 }
